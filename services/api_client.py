494e19493be1f86fc265042fcca6249a5da08a05
KoushikRama <krama1673.pro@gmail.com>
2026-04-14 13:01:59 -0400

    Force serialization

--- services/api_client.py
+++ services/api_client.py
@@ -1,10 +1,11 @@
 import requests
-
+import json
 def call_api(payload, backend, urls):
     
     API_URL = urls["fastapi"] if backend == "FastAPI" else urls["sagemaker"]
 
-    response = requests.post(API_URL, json=payload)
+
+    response = requests.post(API_URL, json=json.loads(json.dumps(payload)))
 
     if response.status_code != 200:
         raise Exception(f"API Error: {response.text}")
